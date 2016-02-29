f958a5d6b052a1ccd262f326166c1b34e7c9956f
Sam Vary <sam.vary@gmail.com>
2016-02-29 13:10:57 -0500

    wasn't quite working before, now drop item seems to be fixed everywhere

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,6 @@ class ItemsController < ApplicationController
 		@item = Item.find(params[:id])
 		@item.character_id = nil
 		@item.save
-		# binding.pry
 		redirect_to :back, notice: "you've dropped #{@item.name}"
 	end
 
@@ -21,8 +20,6 @@ class ItemsController < ApplicationController
 		respond_to do |format|
 			if @location.items.include?(pickup)
 				@character.items << pickup
-				@location.items.delete(pickup)
-				pickup.location_id = nil
 				format.html { redirect_to :back, notice: "You've picked up the #{pickup.name}!"}
 			else
 				format.html { redirect_to :back, alert: "Sorry, that item either isn't here, you've already grabbed it, or you've typed the wrong name. Try again!"}
