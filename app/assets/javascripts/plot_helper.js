9184624d04a4a51c8919fa58479e21c0e5933703
Sam Vary <sam.vary@gmail.com>
2016-03-23 15:21:57 -0400

    adding append methods to the main plot, scroll doesn't quite work perfectly yet though

--- app/assets/javascripts/plot_helper.js
+++ app/assets/javascripts/plot_helper.js
@@ -2,16 +2,28 @@ $(function(){
 
 	var counter = 0;
 
-	var currentMainPlot = $("div#text").text();
+	var currentMainPlot = $("#location-plot").text();
 
 	var sentences = currentMainPlot.split(". ");
+
+	$("#plot-text").append('<div class="well"><p id="current-sentence">' + sentences[counter] + "." + '</p></div>')
 
 	$('#current-sentence').text(sentences[counter] + ".");
 
 	$('#next').click(function () {
 
-		counter = (counter + 1) % sentences.length;
-		
-		$('#current-sentence').text(sentences[counter] + ".");
+
+			if (counter < sentences.length) {
+
+
+				counter = (counter + 1) % sentences.length;
+				
+				$('#plot-text').append('<div class="well"><p id="current-sentence">' + sentences[counter] + "." + '</p></div>');
+
+				var element = document.getElementById("current-sentence");
+				element.scrollIntoView();
+		}
 	});
 });
+
+
